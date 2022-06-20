51ba8cf9333249feeab69c47f29812d622525eef
47425 <474254903@qq.com>
2022-06-20 18:55:34 +0800

    add getNotifications

--- api/v1/user.js
+++ api/v1/user.js
@@ -496,7 +496,7 @@ module.exports = function (app) {
     let lastTime = params.lastTime ?params.lastTime:moment().subtract(30, "days").valueOf()
     let Notification = ctx.model("notification")
     let User = ctx.model("user")
-    let q = {accountId: accountId, "$or":[{type:"comment"},{type:"post"},{'options.At':accountId}]}
+    let q = {"$or":[{accountId: accountId, "$or":[{type:"comment"},{type:"post"}]},{'options.At':accountId}]}
     if (lastTime) {
       q['createAt'] = {$gte: lastTime}
     }
