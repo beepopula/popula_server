4ff00b3e0b91f2b2e36b1d28e0a7034a0046c7d4
wcs <cswang.founder.com.cn>
2022-06-20 16:19:34 +0800

    add follows

--- api/v1/user.js
+++ api/v1/user.js
@@ -521,7 +521,7 @@ module.exports = function (app) {
       comments[i]['data']['type']='comment'
       comments[i]['data']['likes'] = likes;
       comments[i]['data']['count'] = likes.length;
-      comments[i]['data']['At']=comments[i]['options']&&comments[i]['options'].length>0?comments[i]['options']:[]
+      comments[i]['data']['At']=comments[i]['comment']&&comments[i]['comment']['options']&&comments[i]['comment']['options'].length>0?comments[i]['comment']['options']:[]
       if (comments[i]['type']=='post'){
         if (likes.length!=0){
           n.push(comments[i])
