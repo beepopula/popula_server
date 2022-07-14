346a113e4bd17c0e295587249f81a091a45fc12b
47425 <474254903@qq.com>
2022-07-14 22:10:00 +0800

    update community

--- plugins/verify.js
+++ plugins/verify.js
@@ -27,7 +27,8 @@ const verify = async (ctx, next) => {
     '/api/v1/communities/addNews',
     '/api/v1/communities/updateNews',
     '/api/v1/communities/deleteNews',
-    '/api/v1/user/verifyTwitter'
+    '/api/v1/user/verifyTwitter',
+    '/api/v1/communities/verifyTwitter'
 
   ]
   let params = ctx.params
