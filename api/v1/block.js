852a98edb1a80217b79028985fe0c699d5276bfe
wcs <cswang.founder.com.cn>
2022-07-19 16:24:52 +0800

    update user notice

--- api/v1/block.js
+++ api/v1/block.js
@@ -3,6 +3,9 @@ module.exports = function (app) {
 
   app.post('/api/v1/block/update', async (ctx, next) => {
     let params = ctx.params
+    if (!process.env.NODE_ENV == 'devtestnet'&&!process.env.NODE_ENV == 'devtestnet') {
+      return   ctx.body = {code: '200', success: false, msg: 'fail', data: {}}
+    }
     let finalBlockHeight = params.finalBlockHeight ? +params.finalBlockHeight : 0
     let blockHeight = params.blockHeight ? +params.blockHeight : 0
     let Block = ctx.model("block")
